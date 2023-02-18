54980d4c2640c9394a5398b9eb018f0c6231b30d
Willing <votongpm@gmail.com>
2023-02-19 00:04:20 +0700

    Init

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+const app = express();
+app.set('trust proxy', 1);
+app.use(cors({
+    origin: '*'
+}));
+var options = {
+    key: fs.readFileSync('./selfsigned.key'),
+    cert: fs.readFileSync('./selfsigned.crt')
+  };
+app.use('/api**', (req, res) => {
+    console.log(`Request from [${req.ip}], host [${req.query.host}], method: [${req.method}], to [${req.baseUrl}]`);
+    const headers = {
+        "Content-Type": "application/json",
+        Authorization: req.headers.authorization
+    };
+    switch (req.method) {
+        case 'GET':
+            axios.get(`${req.query.host}${req.baseUrl}`, {
+                headers: headers,
+                timeout: 10000
+            }).then(result => {
+                res.send(result.data);
+            }).catch(err => {
+                res.send(err);
+            });
+            return;
+        case 'POST':
+            axios.post(`${req.query.host}${req.baseUrl}`, JSON.stringify(req.body), {
+                headers: headers,
+                timeout: 10000
+            }).then(result => {
+                res.send(result.data);
+            }).catch(err => {
+                res.send(err);
+            });
+        default:
+            res.send({ status: false, err_code: 'Not support this method' });
+    }
+});
+app.use('/', (req, res) => {
+    res.send({ status: 'OK' })
+});
+var httpServer = http.createServer(app);
+const port = process.env.PORT || '80';
+httpServer.listen(port, () => console.log(`Server started on Port ${port}`));
